bb7d89a4e32d2799925fe604e8685c9ec908ed1e
Rajesh Mallempati <mallempati.rajesh@siemens.com>
2024-08-13 13:04:29 +0530

    fix(character_driver): fix the segmentation fault from user space program
    Getting the segmentation fault due to improper data type mismatch
    between the userspace program and the driver code
    
    Fixed the below error in the character driver
    [ 2032.200617] queue[3460]: segfault at 0 ip 0000000000000000 sp 00007ffd4cf68680 error 14 in queue[560276416000+1000] likely on CPU 3 (core 3, socket 0)
    [ 2032.200630] Code: Unable to access opcode bytes at 0xffffffffffffffd6.
    
    Signed-off-by: Rajesh Mallempati <rajeshmallempati9140@gmail.com>

--- character/test_scripts/test_wait_queue.c
+++ character/test_scripts/test_wait_queue.c
@@ -3,14 +3,14 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
+#include <string.h>
 #define DEVICE_NAME "/dev/my_cdev"
 
 int main() {
     int fd;
     pid_t pid;
-    int write_val = 1; // Value to write to the driver
-    int read_val;      // Value read from the driver
+    int8_t write_val[1024]; // Value to write to the driver
+    int8_t read_val[1024];      // Value read from the driver
 
     // Open the device file
     printf("Opening Driver\n");
@@ -31,19 +31,21 @@ int main() {
     if (pid == 0) {
         // Child process: Try to read from the device and go to sleep
         printf("Child process (PID %d) trying to read...\n", getpid());
-        sleep(2); // as it a test application to test the wait queue wait to make the writing operation
-        if (read(fd, &read_val, sizeof(read_val)) < 0) {
+        sleep(10); // as it a test application to test the wait queue wait to make the writing operation
+        if (read(fd, read_val, strlen(read_val)+1) < 0) {
             perror("Read failed");
         } else {
-            printf("Child process (PID %d) read value: %d\n", getpid(), read_val);
+            printf("Child process (PID %d) read value: %s\n", getpid(), read_val);
         }
         close(fd);
         return 0;
     } else {
         // Parent process: Sleep briefly to ensure child sleeps first, then write
         //sleep(2); // Ensure the child has time to sleep
+        printf("Enter the value for writing"); 
+        scanf("  %[^\t\n]s", write_val);
         printf("Parent process (PID %d) writing to wake up the child...\n", getpid());
-        if (write(fd, &write_val, sizeof(write_val)) < 0) {
+        if (write(fd, write_val, strlen(write_val)+1) < 0) {
             perror("Write failed");
         }
         // Wait for the child process to finish
